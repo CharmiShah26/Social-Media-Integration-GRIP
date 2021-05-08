4a9dfdf7a097400710af83386e478a8104bf5345
Your Name <you@example.com>
2021-05-08 17:08:32 +0530

    first

---
+++ lib/widget/TwitterSignupwidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_signin_example/provider/google_sign_in.dart';
+import 'package:google_signin_example/widget/twitter.dart';
+import 'package:provider/provider.dart';
+
+class TwitterSignupButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.all(4),
+        child: OutlineButton.icon(
+          label: Text(
+            'Sign In With Twitter',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          shape: StadiumBorder(),
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          highlightedBorderColor: Colors.black,
+          borderSide: BorderSide(color: Colors.black),
+          textColor: Colors.black,
+          icon: FaIcon(FontAwesomeIcons.twitter, color: Colors.blue),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Twitter()),
+            );
+          },
+        ),
+      );
+}
---
+++ lib/widget/twitter.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+
+class Twitter extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<Twitter> {
+  static final TwitterLogin twitterLogin = new TwitterLogin(
+    consumerKey: '2U9wqfh5e0o0qdnV4X2ORnxmN',
+    consumerSecret: 'BOEb7DiQqGMLMIlHjfK3eRZXRmo5eA5sixnErZVu4jxOWv7xMa',
+  );
+
+  String _message = 'Logged out.';
+
+  void _login() async {
+    final TwitterLoginResult result = await twitterLogin.authorize();
+    String newMessage;
+
+    switch (result.status) {
+      case TwitterLoginStatus.loggedIn:
+        newMessage =
+            'Logged in! userID: ${result.session.userId}\nusername: ${result.session.username}';
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+        newMessage = 'Login cancelled by user.';
+        break;
+      case TwitterLoginStatus.error:
+        newMessage = 'Login error: ${result.errorMessage}';
+        break;
+    }
+
+    setState(() {
+      _message = newMessage;
+    });
+  }
+
+  void _logout() async {
+    await twitterLogin.logOut();
+
+    setState(() {
+      _message = 'Logged out.';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Login through Twitter'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(_message),
+              new RaisedButton(
+                child: new Text('Log in'),
+                onPressed: _login,
+              ),
+              new RaisedButton(
+                child: new Text('Log out'),
+                onPressed: _logout,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
